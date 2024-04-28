5fbb8d7342109ff5bea153c02f5636f28f82f5fe
kylepartridge <kylepartridge99@gmail.com>
2024-04-28 14:32:20 -0400

    Initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Name: Kyle Partridge
+# Date: 01/24/2024
+# Lab 1
+
+name = input("Hello. What is your name? ")
+print(f"It's nice to meet you, {name}.", end = " ")
+age = input("How old are you? ")
+print(f"I see that you are still quite young at only {age}.")
+place = input("Where do you live? ")
+print(f"Wow! I've always wanted to go to {place}. Thanks for chatting with me. Bye!")
+
